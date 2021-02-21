21f3634e30db8a370c9722e6c8a093f223867071
Carlo Santos <carlo@snapit.solutions>
2021-02-21 00:44:42 -0600

    add a working version of linking the social links

--- script.js
+++ script.js
@@ -10,17 +10,18 @@ let user = {
 function updateUser(user) {
   let username = document.querySelector('#username input').value;
   let role = Array.from(document.querySelectorAll('[name="role"]')).filter(x => x.checked)[0].value;
+  
   let languagesArray = Array
     .from(document.querySelectorAll('[name="languages"]'))
     .filter(checkbox => checkbox.checked)
-    .map(checkbox => checkbox.value)
-  let socialLinksArray = Array.from(document.querySelectorAll('[name="social_links')).filter(x => x.value != "");
-  // let postInfo = document.querySelector('.post')
+    .map(checkbox => checkbox.value);
 
-  socialLinksArray = socialLinksArray.map(textField => textField.value);
+  let socialLinksArray = Array
+    .from(document.querySelectorAll('[name="social_links'))
+    .filter(x => x.value != "")
+    .map(textField => textField.value);
 
   let _user = user
-
   _user.username = username || user.username;
   _user.role = role || user.role;
   _user.languages = languagesArray || user.languages;
@@ -29,15 +30,12 @@ function updateUser(user) {
   return _user
 }
 
-function updateUserUI (user) {
-  console.log(user);
-  console.log(user.languagesArray);
+function updateUserUI () {
   document.querySelector('#post_username').innerHTML = user.username;
   document.querySelector('#post_role').innerHTML = user.role;
   document.querySelector('#post_languages').innerHTML = user.languages;
-  // document.querySelector('#post_social_links').innerHTML = user.socialLinksArray;
-
   displaySocialMedia();
+  document.querySelector('#signupform').reset();
 }
 
 function handleUserChanges (e) {
@@ -49,44 +47,21 @@ function handleUserChanges (e) {
 }
 
 function displaySocialMedia() {
-  // add the icon dynamically 
-  // and have the icon have an href of the captured field
   let postInfo = document.querySelector('.post')
   let nodelistOfSocialLinks = document.getElementsByName("social_links");
-  // return nodelistOfSocialLinks;
-  // let github = document.querySelector('#d_github');
-  // let discord = document.querySelector('#d_discord');;
-  // let instagram = document.querySelector('#d_instagram');
-  // let slack = document.querySelector('#d_slack');
   let icon = "";
   let present = false;
 
-  // if this social field is not empty
-  // remove the hidden field on the related social media field on the right
-
-  // when there are contents within remove the hidden tag
   nodelistOfSocialLinks.forEach(socialField => {
     if (socialField.value.length > 0) {
       present = true;
-      // github.classList.remove("hidden");
       icon = document.querySelector(`#${"d_"+ socialField.id}`)
       icon.classList.remove("hidden");
       let linkUrl = socialField.value;
-      // console.log("d_" + socialField.id);
       icon.href = linkUrl;
-
-    } else {
-      console.log("Nothing Here")
     }
-    console.log(socialField.value.length);
-    console.log(socialField.id)
-    console.log(socialField.value)
   })
 
-  // What I'm thinking is matching the id with regular expressions
-  // or i can concatenate what i would have as the id of the display element
-
-  // Select the d_github id and remove the classname hidden
    if (postInfo.classList.contains('hidden')) {
     postInfo.classList.remove('hidden');
   }
